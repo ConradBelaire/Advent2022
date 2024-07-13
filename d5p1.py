279ca10b4bdb143f81d3a762fcbfb8815f4790d9
Conrad Belaire <cbelaire@gmail.com>
2024-07-12 22:10:25 -0400

    d5p1

---
+++ d5p1.py
@@ -0,0 +1,28 @@
+import re
+f = open("d5input.txt", "r")
+result = ""
+
+lines = f.readlines()
+linebreak = lines.index("\n")
+numStacks = int(lines[linebreak - 1][-3])
+stacks = [[] for x in range(numStacks)]
+
+for i in range(linebreak-2, -1, -1):
+    for j in range(numStacks):
+        if lines[i][1+j*4] == " ":
+            continue
+        #print(i, j, lines[i][1+j*4])
+        stacks[j].append(lines[i][1+j*4])
+
+print(stacks)
+
+for line in lines[linebreak+1:]:
+    command = list(map(int, re.findall(r"\d+", line)))
+    #print(command)
+    for i in range(0, command[0]):
+        stacks[command[2]-1].append(stacks[command[1]-1].pop())
+    print(stacks)
+
+for stack in stacks:
+    result += stack.pop()
+print(result)
